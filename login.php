8d0a3e77e0e5ab655ff0d0b21657abe0c5ae7bf0
mmtiks <mihkeltiks@hotmail.com>
2022-06-30 12:56:46 +0300

    shift

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 <head>
     <title> Login</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://accounts.google.com/gsi/client" async defer on:load={googleLoaded}></script>
 </head>
 
 <body>
@@ -24,12 +25,53 @@
                 </div>
                 <p id="message"></p>
                 <button type="submit" name="submit" class="btn btn-primary">Login</button>
+
+                <div id="g_id_onload"
+                    data-client_id="243917720407-ehou79d0bqm0853t2fc0d5fo0fqc709f.apps.googleusercontent.com"
+                    data-callback="handleCredentialResponse">
+                </div>
+                <div class="g_id_signin" data-on-success="onSignIn" data-type="standard"></div>
+
             </form>
             <a href="register.php">register </a>
         </div>
     </div>
     <script src="script.js"></script>
 </body>
+<script>
+    import { onMount } from 'svelte';
+    let googleReady = false;
+    let mounted = false;
+
+    onMount(() => {
+        mounted = true;
+        if (googleReady) {
+            displaySignInButton()
+        }
+    });
+
+    function googleLoaded() {
+        googleReady = true;
+        if (mounted) {
+            displaySignInButton()
+        }
+    }
+
+    function displaySignInButton() {
+        google.accounts.id.initialize({
+            client_id: "x.apps.googleusercontent.com",
+            callback: handleCredentialResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("buttonDiv"),
+            { theme: "outline", size: "large" }  // customization attributes
+        );
+        google.accounts.id.prompt(); // also display the One Tap dialog
+    }
+        google.accounts.id.prompt()}
+    function onSignIn(googleUser){
+        console.log('User is' + JSON.stringify(googleUser.getBasicProfile));
+    }
 
 </html>
 
